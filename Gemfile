b1d4e0c402b76a2502eada636e0c1af9ee9620a0
Matt Patterson <matt@reprocessed.org>
2010-06-19 20:38:53 +0100

    Poking at flickr auth

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.0.beta4'
 
 # Use unicorn as the web server
 gem 'unicorn', :group => :development
+gem 'heroku', :group => :development
 
 # Deploy with Capistrano
 # gem 'capistrano'
--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,2 +1,10 @@
 class FlickrController < ApplicationController
+  def show
+    frob = flickr.auth.getFrob
+    @auth_url = FlickRaw.auth_url :frob => frob, :perms => 'write'
+  end
+  
+  def create
+    
+  end
 end
