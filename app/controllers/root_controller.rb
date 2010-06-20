1d3a82a1d8ac9bf162bcbb51bbb4713222b85635
Matt Patterson <matt@reprocessed.org>
2010-06-20 12:07:12 +0100

    Added root-map-overlay KML

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,9 +1,15 @@
 class RootController < ApplicationController
+  respond_to :html, :kml, :rss
+  
   def index
     unless authorized_user?
       @geotagged_photos = []
     else
-      @geotagged_photos = flickr.photos.search :user_id => session[:flickr_nsid], :has_geo => 'true', :auth => session[:flickr_auth_token], :per_page => 10
+      @geotagged_photos = flickr.photos.search :user_id => session[:flickr_nsid], :has_geo => 'true', :per_page => 10, :auth => session[:flickr_auth_token]
     end
+  end
+  
+  def overlay
+    @dirt_tagged_photos = flickr.photos.search :machine_tags => "soil:pH=", :extras => "geo,machine_tags,owner_name,path_alias", :per_page => 500, :auth => session[:flickr_auth_token]
   end
 end
