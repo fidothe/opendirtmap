406126f0a61a4ebed7b908b2c7826d78b78a5738
Paul Mison <misonp+git@gmail.com>
2010-06-20 12:37:27 +0100

    Put direct auth link on front page with nicer text

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,12 +4,17 @@ class RootController < ApplicationController
   def index
     unless authorized_user?
       @geotagged_photos = []
+
+      frob = flickr.auth.getFrob
+      @auth_url = FlickRaw.auth_url :frob => frob, :perms => 'write'
     else
       @geotagged_photos = flickr.photos.search :user_id => session[:flickr_nsid], :has_geo => 'true', :per_page => 10, :auth => session[:flickr_auth_token]
     end
+
   end
   
   def overlay
     @dirt_tagged_photos = flickr.photos.search :machine_tags => "soil:pH=", :extras => "geo,machine_tags,owner_name,path_alias", :per_page => 500, :auth => session[:flickr_auth_token]
   end
+
 end
