87c55d7049d458839dc13dff432e38be2b2ecf3b
Matt Patterson <matt@reprocessed.org>
2010-06-20 11:22:28 +0100

    Abstracted flickr authed user checker to helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  def authorized_user?
+    !session[:flickr_auth_token].blank?
+  end
+  helper_method :authorized_user?
 end
